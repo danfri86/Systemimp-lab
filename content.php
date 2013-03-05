32af27b3406f9360d2a40dbbbbb162263e4e8e90
Daniel Friberg <dfriberg86@gmail.com>
2013-03-05 13:39:17 +0100

    Doc för lab 3 och nya bilder, samt cretaimages.php skapad

--- content.php
+++ content.php
@@ -24,152 +24,152 @@
 				<ul id="table">
 					<li>
 						<ul>
-							<li><img src="image/image1.jpg" alt="Bild" /></li>
-							<li><img src="image/image2.jpg" alt="Bild" /></li>
-							<li><img src="image/image3.jpg" alt="Bild" /></li>
-							<li><img src="image/image4.jpg" alt="Bild" /></li>
-							<li><img src="image/image5.jpg" alt="Bild" /></li>
-							<li><img src="image/image6.jpg" alt="Bild" /></li>
-							<li><img src="image/image7.jpg" alt="Bild" /></li>
-							<li><img src="image/image8.jpg" alt="Bild" /></li>
-							<li><img src="image/image9.jpg" alt="Bild" /></li>
-							<li><img src="image/image10.jpg" alt="Bild" /></li>
+							<li><img src="image/image1.png" alt="Bild" /></li>
+							<li><img src="image/image2.png" alt="Bild" /></li>
+							<li><img src="image/image3.png" alt="Bild" /></li>
+							<li><img src="image/image4.png" alt="Bild" /></li>
+							<li><img src="image/image5.png" alt="Bild" /></li>
+							<li><img src="image/image6.png" alt="Bild" /></li>
+							<li><img src="image/image7.png" alt="Bild" /></li>
+							<li><img src="image/image8.png" alt="Bild" /></li>
+							<li><img src="image/image9.png" alt="Bild" /></li>
+							<li><img src="image/image10.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
 							
-							<li><img src="image/image11.jpg" alt="Bild" /></li>
-							<li><img src="image/image12.jpg" alt="Bild" /></li>
-							<li><img src="image/image13.jpg" alt="Bild" /></li>
-							<li><img src="image/image14.jpg" alt="Bild" /></li>
-							<li><img src="image/image15.jpg" alt="Bild" /></li>
-							<li><img src="image/image16.jpg" alt="Bild" /></li>
-							<li><img src="image/image17.jpg" alt="Bild" /></li>
-							<li><img src="image/image18.jpg" alt="Bild" /></li>
-							<li><img src="image/image19.jpg" alt="Bild" /></li>
-							<li><img src="image/image20.jpg" alt="Bild" /></li>
+							<li><img src="image/image11.png" alt="Bild" /></li>
+							<li><img src="image/image12.png" alt="Bild" /></li>
+							<li><img src="image/image13.png" alt="Bild" /></li>
+							<li><img src="image/image14.png" alt="Bild" /></li>
+							<li><img src="image/image15.png" alt="Bild" /></li>
+							<li><img src="image/image16.png" alt="Bild" /></li>
+							<li><img src="image/image17.png" alt="Bild" /></li>
+							<li><img src="image/image18.png" alt="Bild" /></li>
+							<li><img src="image/image19.png" alt="Bild" /></li>
+							<li><img src="image/image20.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image21.jpg" alt="Bild" /></li>
-							<li><img src="image/image22.jpg" alt="Bild" /></li>
-							<li><img src="image/image23.jpg" alt="Bild" /></li>
-							<li><img src="image/image24.jpg" alt="Bild" /></li>
-							<li><img src="image/image25.jpg" alt="Bild" /></li>
-							<li><img src="image/image26.jpg" alt="Bild" /></li>
-							<li><img src="image/image27.jpg" alt="Bild" /></li>
-							<li><img src="image/image28.jpg" alt="Bild" /></li>
-							<li><img src="image/image29.jpg" alt="Bild" /></li>
-							<li><img src="image/image30.jpg" alt="Bild" /></li>
+							<li><img src="image/image21.png" alt="Bild" /></li>
+							<li><img src="image/image22.png" alt="Bild" /></li>
+							<li><img src="image/image23.png" alt="Bild" /></li>
+							<li><img src="image/image24.png" alt="Bild" /></li>
+							<li><img src="image/image25.png" alt="Bild" /></li>
+							<li><img src="image/image26.png" alt="Bild" /></li>
+							<li><img src="image/image27.png" alt="Bild" /></li>
+							<li><img src="image/image28.png" alt="Bild" /></li>
+							<li><img src="image/image29.png" alt="Bild" /></li>
+							<li><img src="image/image30.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image31.jpg" alt="Bild" /></li>
-							<li><img src="image/image32.jpg" alt="Bild" /></li>
-							<li><img src="image/image33.jpg" alt="Bild" /></li>
-							<li><img src="image/image34.jpg" alt="Bild" /></li>
-							<li><img src="image/image35.jpg" alt="Bild" /></li>
-							<li><img src="image/image36.jpg" alt="Bild" /></li>
-							<li><img src="image/image37.jpg" alt="Bild" /></li>
-							<li><img src="image/image38.jpg" alt="Bild" /></li>
-							<li><img src="image/image39.jpg" alt="Bild" /></li>
-							<li><img src="image/image40.jpg" alt="Bild" /></li>
+							<li><img src="image/image31.png" alt="Bild" /></li>
+							<li><img src="image/image32.png" alt="Bild" /></li>
+							<li><img src="image/image33.png" alt="Bild" /></li>
+							<li><img src="image/image34.png" alt="Bild" /></li>
+							<li><img src="image/image35.png" alt="Bild" /></li>
+							<li><img src="image/image36.png" alt="Bild" /></li>
+							<li><img src="image/image37.png" alt="Bild" /></li>
+							<li><img src="image/image38.png" alt="Bild" /></li>
+							<li><img src="image/image39.png" alt="Bild" /></li>
+							<li><img src="image/image40.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image41.jpg" alt="Bild" /></li>
-							<li><img src="image/image42.jpg" alt="Bild" /></li>
-							<li><img src="image/image43.jpg" alt="Bild" /></li>
-							<li><img src="image/image44.jpg" alt="Bild" /></li>
-							<li><img src="image/image45.jpg" alt="Bild" /></li>
-							<li><img src="image/image46.jpg" alt="Bild" /></li>
-							<li><img src="image/image47.jpg" alt="Bild" /></li>
-							<li><img src="image/image48.jpg" alt="Bild" /></li>
-							<li><img src="image/image49.jpg" alt="Bild" /></li>
-							<li><img src="image/image50.jpg" alt="Bild" /></li>
+							<li><img src="image/image41.png" alt="Bild" /></li>
+							<li><img src="image/image42.png" alt="Bild" /></li>
+							<li><img src="image/image43.png" alt="Bild" /></li>
+							<li><img src="image/image44.png" alt="Bild" /></li>
+							<li><img src="image/image45.png" alt="Bild" /></li>
+							<li><img src="image/image46.png" alt="Bild" /></li>
+							<li><img src="image/image47.png" alt="Bild" /></li>
+							<li><img src="image/image48.png" alt="Bild" /></li>
+							<li><img src="image/image49.png" alt="Bild" /></li>
+							<li><img src="image/image50.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image51.jpg" alt="Bild" /></li>
-							<li><img src="image/image52.jpg" alt="Bild" /></li>
-							<li><img src="image/image53.jpg" alt="Bild" /></li>
-							<li><img src="image/image54.jpg" alt="Bild" /></li>
-							<li><img src="image/image55.jpg" alt="Bild" /></li>
-							<li><img src="image/image56.jpg" alt="Bild" /></li>
-							<li><img src="image/image57.jpg" alt="Bild" /></li>
-							<li><img src="image/image58.jpg" alt="Bild" /></li>
-							<li><img src="image/image59.jpg" alt="Bild" /></li>
-							<li><img src="image/image60.jpg" alt="Bild" /></li>
+							<li><img src="image/image51.png" alt="Bild" /></li>
+							<li><img src="image/image52.png" alt="Bild" /></li>
+							<li><img src="image/image53.png" alt="Bild" /></li>
+							<li><img src="image/image54.png" alt="Bild" /></li>
+							<li><img src="image/image55.png" alt="Bild" /></li>
+							<li><img src="image/image56.png" alt="Bild" /></li>
+							<li><img src="image/image57.png" alt="Bild" /></li>
+							<li><img src="image/image58.png" alt="Bild" /></li>
+							<li><img src="image/image59.png" alt="Bild" /></li>
+							<li><img src="image/image60.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image61.jpg" alt="Bild" /></li>
-							<li><img src="image/image62.jpg" alt="Bild" /></li>
-							<li><img src="image/image63.jpg" alt="Bild" /></li>
-							<li><img src="image/image64.jpg" alt="Bild" /></li>
-							<li><img src="image/image65.jpg" alt="Bild" /></li>
-							<li><img src="image/image66.jpg" alt="Bild" /></li>
-							<li><img src="image/image67.jpg" alt="Bild" /></li>
-							<li><img src="image/image68.jpg" alt="Bild" /></li>
-							<li><img src="image/image69.jpg" alt="Bild" /></li>
-							<li><img src="image/image70.jpg" alt="Bild" /></li>
+							<li><img src="image/image61.png" alt="Bild" /></li>
+							<li><img src="image/image62.png" alt="Bild" /></li>
+							<li><img src="image/image63.png" alt="Bild" /></li>
+							<li><img src="image/image64.png" alt="Bild" /></li>
+							<li><img src="image/image65.png" alt="Bild" /></li>
+							<li><img src="image/image66.png" alt="Bild" /></li>
+							<li><img src="image/image67.png" alt="Bild" /></li>
+							<li><img src="image/image68.png" alt="Bild" /></li>
+							<li><img src="image/image69.png" alt="Bild" /></li>
+							<li><img src="image/image70.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image71.jpg" alt="Bild" /></li>
-							<li><img src="image/image72.jpg" alt="Bild" /></li>
-							<li><img src="image/image73.jpg" alt="Bild" /></li>
-							<li><img src="image/image74.jpg" alt="Bild" /></li>
-							<li><img src="image/image75.jpg" alt="Bild" /></li>
-							<li><img src="image/image76.jpg" alt="Bild" /></li>
-							<li><img src="image/image77.jpg" alt="Bild" /></li>
-							<li><img src="image/image78.jpg" alt="Bild" /></li>
-							<li><img src="image/image79.jpg" alt="Bild" /></li>
-							<li><img src="image/image80.jpg" alt="Bild" /></li>
+							<li><img src="image/image71.png" alt="Bild" /></li>
+							<li><img src="image/image72.png" alt="Bild" /></li>
+							<li><img src="image/image73.png" alt="Bild" /></li>
+							<li><img src="image/image74.png" alt="Bild" /></li>
+							<li><img src="image/image75.png" alt="Bild" /></li>
+							<li><img src="image/image76.png" alt="Bild" /></li>
+							<li><img src="image/image77.png" alt="Bild" /></li>
+							<li><img src="image/image78.png" alt="Bild" /></li>
+							<li><img src="image/image79.png" alt="Bild" /></li>
+							<li><img src="image/image80.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image81.jpg" alt="Bild" /></li>
-							<li><img src="image/image82.jpg" alt="Bild" /></li>
-							<li><img src="image/image83.jpg" alt="Bild" /></li>
-							<li><img src="image/image84.jpg" alt="Bild" /></li>
-							<li><img src="image/image85.jpg" alt="Bild" /></li>
-							<li><img src="image/image86.jpg" alt="Bild" /></li>
-							<li><img src="image/image87.jpg" alt="Bild" /></li>
-							<li><img src="image/image88.jpg" alt="Bild" /></li>
-							<li><img src="image/image89.jpg" alt="Bild" /></li>
-							<li><img src="image/image90.jpg" alt="Bild" /></li>
+							<li><img src="image/image81.png" alt="Bild" /></li>
+							<li><img src="image/image82.png" alt="Bild" /></li>
+							<li><img src="image/image83.png" alt="Bild" /></li>
+							<li><img src="image/image84.png" alt="Bild" /></li>
+							<li><img src="image/image85.png" alt="Bild" /></li>
+							<li><img src="image/image86.png" alt="Bild" /></li>
+							<li><img src="image/image87.png" alt="Bild" /></li>
+							<li><img src="image/image88.png" alt="Bild" /></li>
+							<li><img src="image/image89.png" alt="Bild" /></li>
+							<li><img src="image/image90.png" alt="Bild" /></li>
 						</ul>
 					</li>
 					
 					<li>
 						<ul>
-							<li><img src="image/image91.jpg" alt="Bild" /></li>
-							<li><img src="image/image92.jpg" alt="Bild" /></li>
-							<li><img src="image/image93.jpg" alt="Bild" /></li>
-							<li><img src="image/image94.jpg" alt="Bild" /></li>
-							<li><img src="image/image95.jpg" alt="Bild" /></li>
-							<li><img src="image/image96.jpg" alt="Bild" /></li>
-							<li><img src="image/image97.jpg" alt="Bild" /></li>
-							<li><img src="image/image98.jpg" alt="Bild" /></li>
-							<li><img src="image/image99.jpg" alt="Bild" /></li>
-							<li><img src="image/image100.jpg" alt="Bild" /></li>
+							<li><img src="image/image91.png" alt="Bild" /></li>
+							<li><img src="image/image92.png" alt="Bild" /></li>
+							<li><img src="image/image93.png" alt="Bild" /></li>
+							<li><img src="image/image94.png" alt="Bild" /></li>
+							<li><img src="image/image95.png" alt="Bild" /></li>
+							<li><img src="image/image96.png" alt="Bild" /></li>
+							<li><img src="image/image97.png" alt="Bild" /></li>
+							<li><img src="image/image98.png" alt="Bild" /></li>
+							<li><img src="image/image99.png" alt="Bild" /></li>
+							<li><img src="image/image100.png" alt="Bild" /></li>
 						</ul>
 					</li>
 				</ul>					
