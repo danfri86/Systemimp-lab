fd4e0201003f945326a37deab30d04dc58a67725
Daniel Friberg <dfriberg86@gmail.com>
2013-03-05 10:48:07 +0100

    Små CSS ändringar

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 		<div id="sitefooter">
 			<p> ISGB11 Systemimplementeringsteknik VT13<br />
-				<?php echo $method ?>
+				<?php echo ($method) ?>
 				<!-- <b style="font-size: 20px;">Här skall lösningsmetoden skrivas ut utan inline style!</b> -->
 			</p>
+			
 			<p>
 				<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Sliict" 							height="31" width="88" /></a>
 			</p>	
