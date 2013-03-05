fc6b4b0305306f17daf27f3432425435e03fd020
Daniel Friberg <dfriberg86@gmail.com>
2013-03-05 11:40:59 +0100

    Skapat debug och gjort anrop i header

--- incl/header.php
+++ incl/header.php
@@ -1,10 +1,17 @@
+<?php
+	ob_start();
+	header("Content-Type: application/xhtml+xml; charset=utf-8");
+	include("debug.php");
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="sv" lang="sv">
 
 	<head>
-		<!--meta-->
+		<!--
 		<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+		-->
 		
 		<title>Tillståndslös miljö, PHP och Array</title>	
 		
