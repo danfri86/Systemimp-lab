9d3a974759b1d2b4b7a6fa02d12fb2fcc6460502
Daniel Friberg <dfriberg86@gmail.com>
2013-03-11 13:35:12 +0100

    nav.php uppdaterad

--- incl/nav.php
+++ incl/nav.php
@@ -1,19 +1,19 @@
 <div id="contentnav">
 					<ul>
 						<li>
-							<a href="#">Gömda fält</a>
+							<a href="content.php">Gömda fält</a>
 						</li>
 						
 						<li>
-							<a href="#">Modifierad URL</a>
+							<a href="content.php">Modifierad URL</a>
 						</li>
 						
 						<li>
-							<a href="#">Kakor</a>
+							<a href="content.php">Kakor</a>
 						</li>
 						
 						<li>
-							<a href="#">Sessioner</a>
+							<a href="content.php">Sessioner</a>
 						</li>
 					</ul>
 				</div><!-- /#contentnav -->
