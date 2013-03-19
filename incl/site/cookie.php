0c7b581969282a5d3f559fdd58aa8e1e5efef5ec
Daniel Friberg <dfriberg86@gmail.com>
2013-03-19 13:34:01 +0100

    cookie och session lite uppdaterade men inte klara

--- incl/site/cookie.php
+++ incl/site/cookie.php
@@ -1,6 +1,25 @@
 <?php
 	
 	echo("cookie.php");
+	
+	include("incl/myArray.php");
+	
+	$oMyArray = new MyArray();
+
+	if(isset($_POST["btnAction"])) { // Kontrollera om något kommer in från formuläret. Om något tryckt på knappen btnAction
+		
+		if($_POST["btnAction"] == "Slumpa") { // Om någon tryckt på motsvarande knapp går vi in här
+			$oMyArray->randomArray();
+			echo($oMyArray->array2XHTMLTable());
+		}
+		
+		if($_POST["btnAction"] == "Sortera") { // Om någon tryckt på motsvarande knapp går vi in här
+			$oMyArray->string2Array($_POST["hidArray"]);
+			$oMyArray->sortArray();
+			echo($oMyArray->array2XHTMLTable());
+			$oMyArray->array2String($_POST["hidArray"]);
+		}
+	}
 				
 ?>
 
