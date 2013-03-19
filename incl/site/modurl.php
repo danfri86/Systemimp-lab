31a33a097aa1e5db6af7d73997ec9cf00f592267
Daniel Friberg <dfriberg86@gmail.com>
2013-03-19 13:29:46 +0100

    modurl fixad

--- incl/site/modurl.php
+++ incl/site/modurl.php
@@ -1,10 +1,30 @@
 <?php
 	
 	echo("modurl.php");
+	
+	include("incl/myArray.php");
+	
+	$oMyArray = new MyArray();
+
+	if(isset($_GET["action"])) { // Kontrollera om något kommer in från formuläret. Om något tryckt på knappen btnAction
+		
+		if($_GET["action"] == "Slumpa") { // Om någon tryckt på motsvarande knapp går vi in här
+			$oMyArray->randomArray();
+			echo($oMyArray->array2XHTMLTable());
+		}
+		
+		if($_GET["action"] == "Sortera") { // Om någon tryckt på motsvarande knapp går vi in här
+			$oMyArray->string2Array($_GET["linkArray"]);
+			$oMyArray->sortArray();
+			echo($oMyArray->array2XHTMLTable());
+			$oMyArray->array2String($_GET["linkArray"]);
+		}
+	}
+
 				
 ?>
 
 <div id="buttonlink">
-	<a href="content.php?p=m&amp;action=slumpa&amp;linkArray=">Slumpa</a>
-	<a href="content.php?p=m&amp;action=sortera&amp;linkArray=">Sortera</a>
+	<a href="content.php?p=m&amp;action=Slumpa&amp;linkArray=<?php echo($oMyArray->array2String()); ?>">Slumpa</a>
+	<a href="content.php?p=m&amp;action=Sortera&amp;linkArray=<?php echo($oMyArray->array2String()); ?>">Sortera</a>
 </div>
