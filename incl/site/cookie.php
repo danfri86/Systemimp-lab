2f2c11a0995b77e6a014227307f76f20ddba1e5e
Daniel Friberg <dfriberg86@gmail.com>
2013-03-25 11:37:54 +0100

    Cookies filen fungerar. Är klar

--- incl/site/cookie.php
+++ incl/site/cookie.php
@@ -5,22 +5,31 @@
 	include("incl/myArray.php");
 	
 	$oMyArray = new MyArray();
-
+	
+	// Ta bort kakan när vektorn är sorterad
+	// setcookie("kaka", "", time() - 3600);
+	
+	if(isset($_COOKIE["kaka"])) {
+		$oMyArray->string2array($_COOKIE["kaka"]);
+	}
+	else {
+		setcookie("kaka", $oMyArray->array2String());
+	}
+	
 	if(isset($_POST["btnAction"])) { // Kontrollera om något kommer in från formuläret. Om något tryckt på knappen btnAction
 		
 		if($_POST["btnAction"] == "Slumpa") { // Om någon tryckt på motsvarande knapp går vi in här
 			$oMyArray->randomArray();
 			echo($oMyArray->array2XHTMLTable());
+			setcookie("kaka", $oMyArray->array2String());
 		}
 		
 		if($_POST["btnAction"] == "Sortera") { // Om någon tryckt på motsvarande knapp går vi in här
-			$oMyArray->string2Array($_POST["hidArray"]);
+			$oMyArray->string2array($_COOKIE["kaka"]);
 			$oMyArray->sortArray();
 			echo($oMyArray->array2XHTMLTable());
-			$oMyArray->array2String($_POST["hidArray"]);
 		}
-	}
-				
+	}				
 ?>
 
 <form id="buttonlink" method="post" action="content.php?p=c">
